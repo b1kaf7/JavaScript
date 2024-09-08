85484b52cdff1fdca33f7371e8d5cdee19f4eb58
Binamra Kafle <161001492+b1kaf7@users.noreply.github.com>
2024-09-08 20:55:18 +0545

    1st commit
    Click the button to change text

---
+++ Change Text/index.js
@@ -0,0 +1,5 @@
+// Function to change the text content of the h1 element
+function changeText() {
+    const header = document.getElementById('header-text');
+    header.textContent = "Hahaha, you changed the text!";
+}
